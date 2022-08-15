3f9a8afd9c6bcb8353c25fa5e623a6177d2a3a1b
Phan Nhật Huy <nhathuy7080zz@gmail.com>
2022-08-15 09:09:58 +0700

    Add files via upload

---
+++ Pi.py
@@ -0,0 +1,63 @@
+#Ultimate Pi Calculator
+import time
+
+print('██╗   ██╗██╗  ████████╗██╗███╗   ███╗ █████╗ ████████╗███████╗    ██████╗ ██╗     ██████╗ █████╗ ██╗      ██████╗██╗   ██╗██╗      █████╗ ████████╗ ██████╗ ██████╗ ')
+print('██║   ██║██║  ╚══██╔══╝██║████╗ ████║██╔══██╗╚══██╔══╝██╔════╝    ██╔══██╗██║    ██╔════╝██╔══██╗██║     ██╔════╝██║   ██║██║     ██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗')
+print('██║   ██║██║     ██║   ██║██╔████╔██║███████║   ██║   █████╗      ██████╔╝██║    ██║     ███████║██║     ██║     ██║   ██║██║     ███████║   ██║   ██║   ██║██████╔╝')
+print('██║   ██║██║     ██║   ██║██║╚██╔╝██║██╔══██║   ██║   ██╔══╝      ██╔═══╝ ██║    ██║     ██╔══██║██║     ██║     ██║   ██║██║     ██╔══██║   ██║   ██║   ██║██╔══██╗')
+print('╚██████╔╝███████╗██║   ██║██║ ╚═╝ ██║██║  ██║   ██║   ███████╗    ██║     ██║    ╚██████╗██║  ██║███████╗╚██████╗╚██████╔╝███████╗██║  ██║   ██║   ╚██████╔╝██║  ██║')
+print(' ╚═════╝ ╚══════╝╚═╝   ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝    ╚═╝     ╚═╝     ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝')
+
+fi = str(input('Directory to put Pi.txt in: ') + '\Pi.txt')
+
+f = open(fi, mode = 'w')
+
+def calcPi(limit):
+
+        q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
+
+        decimal = limit
+        counter = 0
+
+        while counter != decimal + 1:
+                if 4 * q + r - t < n * t:
+                        yield n
+                        if counter == 0:
+                                yield '.'
+                        if decimal == counter:
+                                f.write('')
+                                break
+                        counter += 1
+                        nr = 10 * (r - n * t)
+                        n = ((10 * (3 * q + r)) // t) - 10 * n
+                        q *= 10
+                        r = nr
+                else:
+                        nr = (2 * q + r) * l
+                        nn = (q * (7 * k) + 2 + (r * l)) // (t * l)
+                        q *= k
+                        t *= l
+                        l += 2
+                        k += 1
+                        n = nn
+                        r = nr
+
+pi_digits = calcPi(int(input('Enter the number of decimals to calculate to: ')))
+
+starttime = time.time()
+lasttime = starttime
+
+def main():
+        print('Calculating...')
+        for d in pi_digits:
+                f.write(str(d))
+  
+if __name__ == '__main__':
+        main()
+
+print('Recounting...')
+f = open(fi, mode = 'r')
+l = f.read()
+print('The number of decimal digits is: ', len(l)-2)
+print('Finished in', round((time.time() - lasttime), 3), 'seconds')
+f.close()
